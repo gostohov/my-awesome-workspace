a4c819dc24149075613690c330671c4ee9337cec
Your Name <you@example.com>
2026-01-27 00:14:01 +0300

    Добавил Keycloak в docker-compose.yml

--- docker/postgres-init/01-create-databases.sql
+++ docker/postgres-init/01-create-databases.sql
@@ -6,3 +6,12 @@ GRANT ALL PRIVILEGES ON DATABASE my_awesome_workspace_db TO my_awesome_workspace
 
 ALTER SCHEMA public OWNER TO my_awesome_workspace_user;
 GRANT ALL PRIVILEGES ON SCHEMA public TO my_awesome_workspace_user;
+
+CREATE DATABASE keycloakdb;
+CREATE USER keycloakdbuser WITH ENCRYPTED PASSWORD 'EqhvLbsPhrkkZjcaGWcV7qT';
+GRANT ALL PRIVILEGES ON DATABASE keycloakdb TO keycloakdbuser;
+
+\connect keycloakdb;
+
+ALTER SCHEMA public OWNER TO keycloakdbuser;
+GRANT ALL PRIVILEGES ON SCHEMA public TO keycloakdbuser;
